e13dc3d160525daeb24c06b4d7cfec6e91f8c586
siva durga <57574795+sivadurga-web@users.noreply.github.com>
2025-05-09 11:42:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ from termcolor import colored
 from fastapi.middleware.cors import CORSMiddleware
 
 # Set up logging
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Load environment variables
